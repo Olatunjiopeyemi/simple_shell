adbbaf859ea0eb69e4a150552cea779a7c183004
Michael Buraimoh <buraimohkolapo@gmail.com>
2023-05-24 12:42:51 -0700

    Update to main.h

--- main.h
+++ main.h
@@ -1,5 +1,5 @@
-#ifndef shell_h
-#define shell_h
+#ifndef main_h
+#define main_h
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -27,10 +27,6 @@
 #define CONVERT_UNSIGNED 2
 
 extern char **environ;
-
-#define INFO_INIT \
-{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
-	0, 0, 0}
 
 /**
  * struct liststr -> a list
@@ -101,99 +97,82 @@ typedef struct passinfo
 	int readfd;
 } info_t;
 
+#define INFO_INIT \
+{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
+        0, 0, 0}
+
 void find_cmd(info_t *);
 void fork_cmd(info_t *);
 int hsh(info_t *, char **);
 int find_builtin(info_t *);
-
 char *dup_chars(char *, int, int);
 char *find_path(info_t *, char *, char *);
 int is_cmd(info_t *, char *);
-
 int loophsh(char **);
-
 int _putfd(char c, int fd);
 int _putsfd(char *str, int fd);
 void _eputs(char *);
 int _eputchar(char);
-
 char *starts_with(const char *, const char *);
 char *_strcat(char *, char *);
 int _strlen(char *);
 int _strcmp(char *, char *);
-
 void _puts(char *);
 int _putchar(char);
 char *_strcpy(char *, char *);
 char *_strdup(const char *);
-
 char *_strncat(char *, char *, int);
 char *_strchr(char *, char);
 char *_strncpy(char *, char *, int);
-
 char **strtow2(char *, char);
 char **strtow(char *, char *);
-
 void ffree(char **);
 void *_realloc(void *, unsigned int, unsigned int);
 char *_memset(char *, char, unsigned int);
-
 int bfree(void **);
-
 int _isalpha(int);
 int _atoi(char *);
 int interactive(info_t *);
 int is_delim(char, char *);
-
 char *convert_number(long int, int, int);
 void remove_comments(char *);
 void print_error(info_t *, char *);
 int print_d(int, int);
 int _erratoi(char *);
-
 int _mycd(info_t *);
 int _myhelp(info_t *);
 int _myexit(info_t *);
-
 int _myalias(info_t *);
 int _myhistory(info_t *);
-
 int _getline(info_t *, char **, size_t *);
 void sigintHandler(int);
 ssize_t get_input(info_t *);
-
 void set_info(info_t *, char **);
 void free_info(info_t *, int);
 void clear_info(info_t *);
-
 int _mysetenv(info_t *);
 int _myunsetenv(info_t *);
 int populate_env_list(info_t *);
 char *_getenv(info_t *, const char *);
 int _myenv(info_t *);
-
 char **get_environ(info_t *);
 int _unsetenv(info_t *, char *);
 int _setenv(info_t *, char *, char *);
-
 int read_history(info_t *info);
 int build_history_list(info_t *info, char *buf, int linecount);
 int renumber_history(info_t *info);
 char *get_history_file(info_t *info);
 int write_history(info_t *info);
-
 size_t print_list_str(const list_t *);
 int delete_node_at_index(list_t **, unsigned int);
 void free_list(list_t **);
 list_t *add_node(list_t **, const char *, int);
 list_t *add_node_end(list_t **, const char *, int);
-
 size_t print_list(const list_t *);
 list_t *node_starts_with(list_t *, char *, char);
 ssize_t get_node_index(list_t *, list_t *);
 size_t list_len(const list_t *);
 char **list_to_strings(list_t *);
-
 int replace_alias(info_t *);
 int replace_vars(info_t *);
 int replace_string(char **, char *);
