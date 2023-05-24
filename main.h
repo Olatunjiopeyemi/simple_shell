2b9adcf3643df71f40be80d7350a4c71395c0cce
Michael Buraimoh <buraimohkolapo@gmail.com>
2023-05-24 12:46:10 -0700

    Update to main.h

--- main.h
+++ main.h
@@ -99,7 +99,7 @@ typedef struct passinfo
 
 #define INFO_INIT \
 {NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
-        0, 0, 0}
+	0, 0, 0}
 
 void find_cmd(info_t *);
 void fork_cmd(info_t *);
